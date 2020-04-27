874cbac8fbe2da3cdb853b897aa425943330409b
cokelopez <cokelopez@gmail.com>
2020-04-27 18:09:43 -0500

    dj-database

--- GPC/settings.py
+++ GPC/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import dj_database_url
 import os
 
 
@@ -160,3 +161,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 LOGIN_REDIRECT_URL = "/"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
